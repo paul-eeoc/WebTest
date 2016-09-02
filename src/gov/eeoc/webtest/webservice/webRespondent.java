f434bab82332e73e81d4122a08062ec59415c348
P.Eddie <paul.eddie@eeoc.gov>
2016-09-02 11:12:23 -0400

    Added java files for Respondent
    la la la.

---
+++ src/gov/eeoc/webtest/webservice/webRespondent.java
@@ -0,0 +1,18 @@
+package gov.eeoc.webtest.webservice;
+
+
+public class webRespondent {
+// new code from another dev
+	
+	public static void main(String[] args) {
+		System.out.println("webRespondent start");
+		
+		test1();
+		System.out.println("webRespondent end");
+	}
+
+	private static void test1() {
+		System.out.println("webRespondent running test1");
+	}
+
+}
