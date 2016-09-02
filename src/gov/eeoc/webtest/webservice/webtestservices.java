f9f9ebbce49065df6f6af3ebd2f4a49dcc33de13
P.Eddie <paul.eddie@eeoc.gov>
2016-09-02 11:17:03 -0400

    All output begins with JW

--- src/gov/eeoc/webtest/webservice/webtestservices.java
+++ src/gov/eeoc/webtest/webservice/webtestservices.java
@@ -5,18 +5,18 @@ public class webtestservices {
 // new code from another dev
 	
 	public static void main(String[] args) {
-		System.out.println("webtestservices start");
+		System.out.println("JWebtestservices start");
 		
 		test1();
 		testClosedPortal();
-		System.out.println("webtestservices end");
+		System.out.println("JWebtestservices end");
 	}
 
 	private static void test1() {
-		System.out.println("webtestservices running test1");
+		System.out.println("JWebtestservices running test1");
 	}
 	private static void testClosedPortal() {
-		System.out.println("webtestservices running test1");
+		System.out.println("JWebtestservices running test1");
 	}
 
 }
