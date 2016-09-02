65ea058915076f475d11fe50eb0e0b45439b8795
P.Eddie <paul.eddie@eeoc.gov>
2016-09-02 08:38:58 -0400

    Added Test programs

---
+++ src/gov/eeoc/webtest/webservice/webtest.java
@@ -0,0 +1,18 @@
+package gov.eeoc.webtest.webservice;
+
+
+public class webtest {
+// new code from another dev
+	
+	public static void main(String[] args) {
+		System.out.println("webtest start");
+		
+		test1();
+		System.out.println("webtest end");
+	}
+
+	private static void test1() {
+		System.out.println("webtest running test1");
+	}
+
+}
--- src/gov/eeoc/webtest/webservice/webtestservices.java
+++ src/gov/eeoc/webtest/webservice/webtestservices.java
@@ -3,5 +3,16 @@ package gov.eeoc.webtest.webservice;
 
 public class webtestservices {
 // new code from another dev
+	
+	public static void main(String[] args) {
+		System.out.println("webtestservices start");
+		
+		test1();
+		System.out.println("webtestservices end");
+	}
+
+	private static void test1() {
+		System.out.println("webtestservices running test1");
+	}
 
 }
